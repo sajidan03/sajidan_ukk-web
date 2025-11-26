8eabc991b83800270034226509aba7bc9df8dee8
sajidan03 <rifansyahsajidan03@gmail.com>
2025-11-26 08:58:55 +0700

    Benerin

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -793,12 +793,11 @@ const WelcomePage: React.FC<WelcomePageProps> = ({
 
                         {/* Right Side - Auth Section */}
                         <div className="hidden md:flex items-center space-x-4">
-                            {/* Cart Icon */}
-                            <button className="relative p-2 text-gray-600 hover:text-[#3862a2] transition duration-200">
+                            {/* <button className="relative p-2 text-gray-600 hover:text-[#3862a2] transition duration-200">
                                 <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
                                 </svg>
-                            </button>
+                            </button> */}
 
                             {/* Always show Logged Out State */}
                             <div className="flex items-center space-x-3">
